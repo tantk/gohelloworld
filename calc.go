ae26305cbfbf3705a5a01872041c65a84c5982fd
tk <tantk7@gmail.com>
2020-11-27 14:32:55 +0800

    edited calc.go

---
+++ calc.go
@@ -0,0 +1,21 @@
+package main
+
+// Add two numbers and return the results
+func Add(a, b int) int {
+	return a + b
+}
+
+// Subtract two numbers and return the result.
+func Subtract(a, b int) int {
+	return a - b
+}
+
+// Multiply two numbers and return the result.
+func Multiply(a, b int) int {
+	return a * b
+}
+
+// Divide two numbers and return the result.
+func Divide(a, b int) float64 {
+	return float64(a / b)
+}
